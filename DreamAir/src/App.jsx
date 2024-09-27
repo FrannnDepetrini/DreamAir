4005f746aaefe448644e95444ca8448fca2847e1
Depeex <fdepeex@gmail.com>
2024-09-27 18:26:22 -0300

    Segundo commit: Carpeta Utils/Colors/Fonts

--- DreamAir/src/App.jsx
+++ DreamAir/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import CardFlight from "./components/cardFlight/cardFlight";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const flight1 = {
+    departure: "ROS Fisherton",
+    arrival: "BSB Presidente",
+    timeArrival: "16:20",
+    timeDeparture: "8:20",
+    duration: "8h 46m",
+    date: "string",
+    totalAmount: "100",
+    priceDefault: "150000",
+    airline: "Emirates",
+    ticketsAvailable: "60",
+  };
 
+  const flight2 = {
+    departure: "BSB Presidente",
+    arrival: "ROS Fisherton",
+    timeArrival: "8:20",
+    timeDeparture: "16:20",
+    duration: "8h 46m",
+    date: "string",
+    totalAmount: "100",
+    priceDefault: "160000",
+    airline: "Emirates",
+    ticketsAvailable: "60",
+  };
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="container_app">
+      <CardFlight flightDeparture={flight1} />
+    </div>
+  );
 }
 
-export default App
+export default App;
