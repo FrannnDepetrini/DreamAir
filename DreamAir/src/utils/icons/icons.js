127343aaf99b9ad28aa6d9b249b68c81b3c821de
Depeex <fdepeex@gmail.com>
2024-09-29 20:28:54 -0300

    Nueva carpeta: utils/icons

---
+++ DreamAir/src/utils/icons/icons.js
@@ -0,0 +1,3 @@
+export { FaRegHeart } from "@react-icons/all-files/fa/FaRegHeart";
+export { IoAirplaneSharp } from "@react-icons/all-files/io5/IoAirplaneSharp";
+export { FaHeart } from "@react-icons/all-files/fa/FaHeart";
