3e28994ad36cf6cfb072a1c5d86aa5b8fd44ef15
Depeex <fdepeex@gmail.com>
2024-09-30 14:06:49 -0300

    Componente Header: Primeros retoques

---
+++ DreamAir/src/components/header/header.jsx
@@ -0,0 +1,28 @@
+import "../header/header.css";
+import { FaUser, IoMenu } from "../../utils/icons/icons";
+
+const Header = () => {
+  const handlerLogin = async () => {};
+  return (
+    <div className="container_header">
+      <div>
+        <div className="container_menu">
+          <IoMenu className="icon" />
+        </div>
+        <div className="container_image">
+          <img
+            style={{ width: "200px" }}
+            src="https://github.com/FrannnDepetrini/DreamAir/blob/main/DreamAir/src/utils/images/LogoDreamAirPH.png?raw=true"
+            alt=""
+          />
+        </div>
+      </div>
+      <div onClick={handlerLogin} className="container_login">
+        <h3>Iniciar sesion</h3>
+        <FaUser className="icon" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
