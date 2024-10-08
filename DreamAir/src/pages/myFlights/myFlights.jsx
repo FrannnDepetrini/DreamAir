25cd3d811e4b2798309e38f564de75a3a2310b5d
Joaquin Tanlongo <joako.tanlon@gmail.com>
2024-10-07 21:01:38 -0300

    pahesMyFlights: arreglos

--- DreamAir/src/pages/myFlights/myFlights.jsx
+++ DreamAir/src/pages/myFlights/myFlights.jsx
@@ -3,7 +3,7 @@ import "../myFlights/myFlights.css";
 const MyFlights = () => {
   return (
     <div className="div-container-page-myflights">
-      <h2 className="h2"> Mis Vuelos</h2>
+      <h2 className="h2"> Mis Viajes</h2>
       <TableUser />
     </div>
   );
